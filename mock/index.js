f96adef5b20b906c9a01aa65b0fc087a44955999
chengyongfei <563837669@qq.com>
2020-02-03 16:51:28 +0800

    master-mock

---
+++ mock/index.js
@@ -0,0 +1,31 @@
+const appData = require('./data.json')
+const proxy = {
+    'GET /api/login': {
+        success: appData.login.success,
+        message: appData.login.message
+    },
+    'GET /api/list': [{
+            id: 1,
+            username: 'kenny',
+            sex: 6
+        },
+        {
+            id: 2,
+            username: 'kenny',
+            sex: 6
+        }
+    ],
+    'POST /api/post': (req, res) => {
+        res.send({
+            status: 'error',
+            code: 403
+        });
+    },
+    'DELETE /api/remove': (req, res) => {
+        res.send({
+            status: 'ok',
+            message: '删除成功！'
+        });
+    }
+}
+module.exports = proxy
